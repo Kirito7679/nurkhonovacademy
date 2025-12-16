4d2d69cd9d6a32b8440ad9cf5b0a8f318c00d74b
Dilmurod Nurkhonov <121294922+Kirito7679@users.noreply.github.com>
2025-12-16 12:38:32 +0500

    fix: Update deploy-ready with corrected React dependencies in vendor-react

--- deploy-ready/assets/useDebounce-DdSuMdMW.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./vendor-react-BKQy8cua.js";function c(e,t=500){const[r,u]=o.useState(e);return o.useEffect(()=>{const n=setTimeout(()=>{u(e)},t);return()=>{clearTimeout(n)}},[e,t]),r}export{c as u};
